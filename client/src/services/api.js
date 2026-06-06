baf662dec66116ec6d208d84689640f59d8cdb23
Rohit Yadav <rohityadavit45@gmail.com>
2026-06-06 23:21:16 +0530

    first commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/leads",
+  baseURL: "https://lead-management-6676.onrender.com/api/leads",
 });
 
 export default API;
